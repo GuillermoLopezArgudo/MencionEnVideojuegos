e59846e88312da40353e58545f641c1d410af862
Guillermo <Guloar@floridauniversitaria.es>
2022-01-22 00:09:13 +0100

    Ejercicio 2 y medio

---
+++ Sesion5/Ejercicio-2'5/ScriptBala.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptBala : MonoBehaviour
+{
+    GameObject nave;
+    SpaceShipScript scriptNave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nave = GameObject.FindGameObjectWithTag("Player");
+        scriptNave = nave.GetComponent<SpaceShipScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {   
+        scriptNave.SetBoolean();
+        Destroy(gameObject);
+    }
+}
