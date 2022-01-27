42e094518c2cfa8947929e006abcaf46c5a21f11
Guillermo <Guloar@floridauniversitaria.es>
2022-01-27 19:47:43 +0100

    Ejercicio-6

---
+++ Sesion5/Ejercicio-6/ScriptAlien.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptAlien : MonoBehaviour
+{
+    public float velocidad = 0.8f;
+    bool boleana = true;
+    bool endGame = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+             
+        InvokeRepeating("Move", 0.8f, velocidad);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Move()
+    {
+        if (endGame == true)
+        {
+            if (boleana == true)
+            {
+
+                if (transform.position.x < 3)
+                {
+                    transform.Translate(Vector2.right);
+                }
+                else
+                {
+                    transform.Translate(Vector2.down);
+                    CancelInvoke();
+                    velocidad -= 0.1f;
+                    boleana = false;
+                    InvokeRepeating("Move", 0.8f, velocidad);
+                }
+            }
+            else if (boleana == false)
+            {
+
+                if (transform.position.x > -3)
+                {
+
+                    transform.Translate(Vector2.left);
+
+                }
+                else
+                {
+                    transform.Translate(Vector2.down);
+                    CancelInvoke();
+                    velocidad -= 0.1f;
+                    boleana = true;
+                    InvokeRepeating("Move", 0.8f, velocidad);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if( collision.gameObject.tag == "Bala")
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            endGame = false;
+        }
+
+    }
+}
