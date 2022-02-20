87bcfd3fed39231e4ae094a495f214b3c4785ee6
Guillermo <Guloar@floridauniversitaria.es>
2022-02-20 18:17:33 +0100

    Ejercicio-6

---
+++ Plataformas_mecion/Ejercicio-6/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float fuerza = 5;
+    Rigidbody2D Player;
+    SpriteRenderer Player2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<Rigidbody2D>();
+        Player2 = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        if (inputX < 0)
+        {
+            Player2.flipX = true;
+            Player.velocity = (transform.right * inputX * fuerza);
+
+        }
+        else
+        {
+            Player2.flipX = false;
+            Player.velocity = (transform.right * inputX * fuerza);
+
+        }
+        
+    }
+}
